796209d6c5f564e0b579f1e154f7b16a84d59696
h0keun <dlghrms95@gmail.com>
2021-05-02 19:40:08 +0900

    email login

--- app/src/main/java/com/tinder/MainActivity.kt
+++ app/src/main/java/com/tinder/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.tinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstancesState: Bundle?) {
+        super.onCreate(savedInstancesState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
