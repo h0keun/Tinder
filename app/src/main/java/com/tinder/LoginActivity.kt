e1ff10da638a8c78efd9431d6ebb7c7fe1ac7b48
h0keun <dlghrms95@gmail.com>
2021-05-02 17:14:51 +0900

    pc change

---
+++ app/src/main/java/com/tinder/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.tinder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+        override fun onCreate(savedInstancesState: Bundle?){
+            super.onCreate(savedInstancesState)
+            setContentView(R.layout.activity_login)
+        }
+
+}
